517552189d3382c47a2b6bdbf8294d42d41136d0
kalosius <kalosius43@gmail.com>
2025-05-17 11:51:23 +0300

    Initial commit

---
+++ ajojo/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import *
+# Register your models here.
+admin.site.register(Category)
+admin.site.register(Product)
+#Provide access to the admin to the sales
+admin.site.register(Sale)
